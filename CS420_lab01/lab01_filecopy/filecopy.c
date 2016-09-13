df82563439b6b7572b61d9d58bde3b6d5510c445
Aaron Walsh <awalsh@localhost.localdomain>
2016-09-13 18:11:34 -0400

    Continuing work. Now have both files opened/created with correct fstats. All that is left is to actually move the data.

--- CS420_lab01/lab01_filecopy/filecopy.c
+++ CS420_lab01/lab01_filecopy/filecopy.c
@@ -46,16 +46,16 @@ int main(int argc, char** argv)
   // Open the source file, be sure to apply appropriate permissions
   // BE SURE TO CHECK FOR ERRORS WHEN OPENING FILES
   // If an error occurs, report the error and terminate the program
-  int fhandle = open(src_file, O_RDONLY);
+  int sfhandle = open(src_file, O_RDONLY);
 
-  if(fhandle < 0)
+  if(sfhandle < 0)
   {
     switch(errno) {
         case ENOENT:
-          printErr("File or directory does not exist\n");
+          printErr("Source file or directory does not exist\n");
         break;
         case EACCES:
-          printErr("Access to that file is denied\n");
+          printErr("Access to source file is denied\n");
         break;
     }
     printErr("Error opening file\n");
@@ -65,33 +65,76 @@ int main(int argc, char** argv)
   // TODO
   // Determine the permission of the source file
   // Again, be sure to check for errors!
+  struct stat  filestats;
+  if(fstat(sfhandle, &filestats) < 0) {
+    switch(errno) {
+      case ENOENT:
+        printErr("File or directory does not exist\n");
+            break;
+      case EACCES:
+        printErr("Access to that file is denied\n");
+            break;
+    }
+    printErr("Error reading file attributes\n");
+    exit(1);
+  }
+
+  if(!S_ISREG(filestats.st_mode)) {
+    printErr("Source filepath is a directory, not a file\n");
+    exit(1);
+  }
 
   // TODO
   // Open the destination file, be sure to apply appropriate permissions
   // BE SURE TO CHECK FOR ERRORS WHEN OPENING FILES
   // If an error occurs, report the error and terminate the program
+  int dfhandle = open(dst_file, O_CREAT | O_WRONLY| O_TRUNC, filestats.st_mode);
 
-
+  if(dfhandle < 0)
+  {
+    switch(errno) {
+      case ENOENT:
+        printErr("Dest file or directory does not exist\n");
+            break;
+      case EACCES:
+        printErr("Access to dest file is denied\n");
+            break;
+    }
+    printErr("Error opening file\n");
+    exit(1);
+  }
 
   // TODO
   // Output a status message here, something like
   // "Copying AAA to BBB"
-
- 
-
+  printStr("Copying file ");
+  printStr(src_file);
+  printStr(" to destination ");
+  printStr(dst_file);
+  printStr("\n");
   // TODO
   // Do the work to actually copy the file here
   // Be efficient and copy CHUNKS of data
   // DO NOT COPY ONE BYTE AT AT TIME
   // DO NOT READ THE ENTIRE FILE INTO MEMORY
   // Again, be sure to check for errors!
+//    while(1){
+//        if(read(sfhandle, data_buf, BUFFER_SIZE)< 0)
+//        {
+//            printErr("Error reading file \n");
+//        }
+//
+//    }
 
 
 
   // TODO
   // Close the source and destination files here
   // As always, check for any errors that may be generated
-
+  if(close(sfhandle) < 0 || close(dfhandle) < 0) {
+    printErr("Error closing files\n");
+    exit(1);
+  }
 
   return 0;
 }
@@ -104,6 +147,6 @@ int printStr(char *str)
 
 int printErr(char *str)
 {
-    //TODO: write to STDOUT
+    //TODO: write to STDERR
     return write(STDERR, str, strlen(str));
 }
