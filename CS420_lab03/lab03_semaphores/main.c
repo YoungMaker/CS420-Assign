ee8b1f2634860e6572ec297b07a80cb65eb56d2a
Aaron Walsh <awalsh@localhost.localdomain>
2016-10-16 14:10:01 -0400

    Added lab 3 (correctly) and worked on arg parsing

--- CS420_lab03/lab03_semaphores/main.c
+++ CS420_lab03/lab03_semaphores/main.c
@@ -17,10 +17,48 @@ int main(int argc, char** argv)
     char* filename = NULL;  // DO NOT MODIFY THIS VARIABLE NAME
     int num_procs = 0;      // DO NOT MODIFY THIS VARIABLE NAME
     int num_threads = 0;    // DO NOT MODIFY THIS VARIABLE NAME
-    
-    
+
+
+
     // TODO: parse arguments
-    
+
+    if(argc != 7 ) {
+        fprintf(stderr, "USAGE : ./main -p <num_procs> -t <num_threads> -f <filename>\n"
+                " -p : the number of processes to create\n"
+                " -t : the number of threads to create per process\n"
+                " -f : the name of the shared file in which to write output\n");
+        return -1; //exit program here, we do not have correct amount of args.
+    }
+
+    int opt;
+    while((opt = getopt(argc, argv, "p:t:f:")) != -1) {
+        switch(opt) {
+            case 'p':
+                //todo: handle num processes
+                num_procs = atoi(optarg);
+                printf("num processes is %d\n", num_procs);
+                break;
+            case 't':
+                //todo: handle num threads
+                num_threads = atoi(optarg);
+                printf("num threads is %d\n", num_threads);
+                break;
+            case 'f':
+                //todo: handle filename
+                filename = optarg;
+                printf("File name is %s\n", filename);
+                break;
+            case '?':
+                fprintf(stderr, "USAGE : ./main -p <num_procs> -t <num_threads> -f <filename>\n"
+                        " -p : the number of processes to create\n"
+                        " -t : the number of threads to create per process\n"
+                        " -f : the name of the shared file in which to write output\n");
+                return -1; //exit program here, we have incorrect args.
+            default:
+                //what is going on here?
+                break;
+        }
+    }
     
     
     // TODO: write initial '0' to file
